56430089ed077d69a13fe414d47cfb1e466d327f
Dev Dixit <devdixitsocial@gmail.com>
2025-08-07 19:10:45 +0530

    modify: try-catch block for better error handling

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,331 +11,387 @@ export const CheckRoute = async (req, res) => {
 } // 10/10
 
 export const UserRegister = async (req, res) => {
-  const { fullName, userEmail, userContactNumber, userRole, password } = req.body;
+  try {
+    const { fullName, userEmail, userContactNumber, userRole, password } = req.body;
 
-  if (!fullName || !userEmail || !userContactNumber || !password) {
-    return res.status(400).json({
-      message: 'All fields are required for registration'
+    if (!fullName || !userEmail || !userContactNumber || !password) {
+      return res.status(400).json({
+        message: 'All fields are required for registration'
+      });
+    }
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const isUserAlreadyExisted = await User.findOne({ userEmail });
+
+    if (isUserAlreadyExisted) {
+      return res.status(400).json({
+        message: 'User already existed. Do login'
+      });
+    }
+
+    const generatedOTP = () => {
+      return Math.floor(100000 + Math.random() * 900000).toString();
+    };
+    const otp = generatedOTP(); // genOTP
+
+    const sessionId = uuidv4(); // genSessionID
+
+    const newUser = {
+      sessionId,
+      fullName,
+      userEmail,
+      userContactNumber,
+      userRole,
+      password,
+      verification_otp: otp
+    }
+
+    await redis.set(`sessionId`, JSON.stringify(newUser), 'EX', 1800)
+      .then(() => { console.log(`User data stored`) })
+      .catch((e) => { console.log(`Error while storing user in redis - ${e}`) });
+    // expire after 1 hour
+    // store data temp
+
+    await transporter.sendMail({
+      from: 'msi.devdixit@gmail.com',
+      to: userEmail,
+      subject: 'Hello from Job Board API',
+      text: `Welcome to the Job Board API. You just created an account with ${userEmail}.`,
+      html: `Your verification otp is <b>${otp}</b>`
+    });
+
+    const decodedToken = await encodeJwt(newUser.sessionId);
+
+    return res
+      .cookie('sessionId', decodedToken, {
+        maxAge: 30 * 60 * 1000,
+        httpOnly: true,
+        secure: true
+      }).status(200)
+      .json({
+        success: true,
+        message: `Welcome ${newUser.fullName}. We just sent you 6 digit OTP for account verification.`,
+      });
+  }
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
     });
   }
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const isUserAlreadyExisted = await User.findOne({ userEmail });
-
-  if (isUserAlreadyExisted) {
-    return res.status(400).json({
-      message: 'User already existed. Do login'
-    });
-  }
-
-  const generatedOTP = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-  };
-  const otp = generatedOTP(); // genOTP
-
-  const sessionId = uuidv4(); // genSessionID
-
-  const newUser = {
-    sessionId,
-    fullName,
-    userEmail,
-    userContactNumber,
-    userRole,
-    password,
-    verification_otp: otp
-  }
-
-  await redis.set(`sessionId`, JSON.stringify(newUser), 'EX', 1800)
-    .then(() => { console.log(`User data stored`) })
-    .catch((e) => { console.log(`Error while storing user in redis - ${e}`) });
-  // expire after 1 hour
-  // store data temp
-
-  await transporter.sendMail({
-    from: 'msi.devdixit@gmail.com',
-    to: userEmail,
-    subject: 'Hello from Job Board API',
-    text: `Welcome to the Job Board API. You just created an account with ${userEmail}.`,
-    html: `Your verification otp is <b>${otp}</b>`
-  });
-
-  const decodedToken = await encodeJwt(newUser.sessionId);
-
-  return res
-    .cookie('sessionId', decodedToken, {
-      maxAge: 30 * 60 * 1000,
-      httpOnly: true,
-      secure: true
-    }).status(200)
-    .json({
-      success: true,
-      message: `Welcome ${newUser.fullName}. We just sent you 6 digit OTP for account verification.`,
-    });
 } // 9/10
 
 export const UserOTPVerification = async (req, res) => {
-  const { otp } = req.body;
+  try {
+    const { otp } = req.body;
 
-  const id = req.cookies.sessionId;
-  const decodedToken = await decodeJwt(id)
+    const id = req.cookies.sessionId;
+    const decodedToken = await decodeJwt(id)
 
-  const user = await redis.get(`sessionId`);
+    const user = await redis.get(`sessionId`);
 
-  if (!id || !user) {
-    return res.status(400).json({
-      message: 'You are not authenticated user or register again'
-    });
+    if (!id || !user) {
+      return res.status(400).json({
+        message: 'You are not authenticated user or register again'
+      });
+    }
+
+    if (!otp) {
+      return res.status(400).json({
+        message: 'Verification OTP is required'
+      });
+    }
+
+    const parsedData = JSON.parse(`${user}`);
+
+    const isUserAuth = parsedData.sessionId === decodedToken.id;
+
+    const checkOTP = otp === parsedData.verification_otp; // vulnerable
+
+    if (!checkOTP) {
+      return res.status(400).json({
+        message: 'Your verification OTP is incorrect'
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(parsedData.password, 10);
+
+    if (isUserAuth) {
+      const newUser = await User.create({
+        fullName: parsedData.fullName,
+        userEmail: parsedData.userEmail,
+        userContactNumber: parsedData.userContactNumber,
+        userRole: parsedData.userRole,
+        password: hashPassword,
+        isUserVerified: true
+      });
+
+      await redis.del(`sessionId`);
+
+      return res.status(201).json({
+        message: 'User registered',
+        user: newUser
+      });
+    }
+    else {
+      return res.status(400).json({
+        message: 'You need to do register again'
+      });
+    }
   }
-
-  if (!otp) {
-    return res.status(400).json({
-      message: 'Verification OTP is required'
-    });
-  }
-
-  const parsedData = JSON.parse(`${user}`);
-
-  const isUserAuth = parsedData.sessionId === decodedToken.id;
-
-  const checkOTP = otp === parsedData.verification_otp; // vulnerable
-
-  if (!checkOTP) {
-    return res.status(400).json({
-      message: 'Your verification OTP is incorrect'
-    });
-  }
-
-  const hashPassword = await bcrypt.hash(parsedData.password, 10);
-
-  if (isUserAuth) {
-    const newUser = await User.create({
-      fullName: parsedData.fullName,
-      userEmail: parsedData.userEmail,
-      userContactNumber: parsedData.userContactNumber,
-      userRole: parsedData.userRole,
-      password: hashPassword,
-      isUserVerified: true
-    });
-
-    await redis.del(`sessionId`);
-
-    return res.status(201).json({
-      message: 'User registered',
-      user: newUser
-    });
-  }
-  else {
-    return res.status(400).json({
-      message: 'You need to do register again'
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
     });
   }
 } // 8.5/10
 
 export const UserLogin = async (req, res) => {
-  const { userEmail, password } = req.body;
+  try {
+    const { userEmail, password } = req.body;
 
-  if (!userEmail || !password) {
-    return res.status(400).json({
-      message: 'All fields are required for login'
+    if (!userEmail || !password) {
+      return res.status(400).json({
+        message: 'All fields are required for login'
+      });
+    }
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const isUserRegistered = await User.findOne({ userEmail: userEmail });
+
+    if (!isUserRegistered) {
+      return res.status(400).json({
+        message: 'Account not found. Do registration first',
+        redirectedTo: '/register'
+      });
+    }
+
+    if (!isUserRegistered.isUserVerified) {
+      return res.status(400).json({
+        message: 'You need to verify your account first before login. Check your email for verification code',
+        timeLimit: '30m'
+      });
+    }
+
+    const decodePassword = await bcrypt.compare(password, isUserRegistered.password);
+
+    if (!decodePassword) {
+      return res.status(400).json({
+        message: 'Invalid email or password',
+        suggestion: 'reset-password'
+      });
+    }
+
+    const encodeToken = await encodeJwt(isUserRegistered._id);
+
+    res.clearCookie('sessionId');
+
+    isUserRegistered.isUserLoggedIn = true
+    await isUserRegistered.save();
+
+    return res
+      .cookie('token', encodeToken, {
+        maxAge: 30 * 60 * 1000,
+        httpOnly: true,
+        secure: true
+      })
+      .status(200)
+      .json({
+        message: `Welcome back, ${isUserRegistered.fullName} 🎈`,
+      });
+  }
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
     });
   }
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const isUserRegistered = await User.findOne({ userEmail: userEmail });
-
-  if (!isUserRegistered) {
-    return res.status(400).json({
-      message: 'Account not found. Do registration first',
-      redirectedTo: '/register'
-    });
-  }
-
-  if (!isUserRegistered.isUserVerified) {
-    return res.status(400).json({
-      message: 'You need to verify your account first before login. Check your email for verification code',
-      timeLimit: '30m'
-    });
-  }
-
-  const decodePassword = await bcrypt.compare(password, isUserRegistered.password);
-
-  if (!decodePassword) {
-    return res.status(400).json({
-      message: 'Invalid email or password',
-      suggestion: 'reset-password'
-    });
-  }
-
-  const encodeToken = await encodeJwt(isUserRegistered._id);
-
-  res.clearCookie('sessionId');
-
-  isUserRegistered.isUserLoggedIn = true
-  await isUserRegistered.save();
-
-  return res
-    .cookie('token', encodeToken, {
-      maxAge: 30 * 60 * 1000,
-      httpOnly: true,
-      secure: true
-    })
-    .status(200)
-    .json({
-      message: `Welcome back, ${isUserRegistered.fullName} 🎈`,
-    });
 } // 9/10
 
 export const UserLogout = async (req, res) => {
-  const user = req.user;
+  try {
+    const user = req.user;
 
-  if (!user) {
-    return res.status(400).json({
-      message: 'User not found'
+    if (!user) {
+      return res.status(400).json({
+        message: 'User not found'
+      });
+    }
+
+    user.isUserLoggedIn = false
+    await user.save();
+
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: true
+    });
+
+    return res.status(200).json({
+      message: 'User logged out successfully'
     });
   }
-
-  user.isUserLoggedIn = false
-  await user.save();
-
-  res.clearCookie('token', {
-    httpOnly: true,
-    secure: true
-  });
-
-  return res.status(200).json({
-    message: 'User logged out successfully'
-  });
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
 } // 9/10
 
 export const UserVerifyEmail = async (req, res) => {
-  const { userEmail } = req.body;
+  try {
+    const { userEmail } = req.body;
 
-  if (!userEmail) {
-    return res.status(400).json({
-      message: 'Email field is required for verification'
+    if (!userEmail) {
+      return res.status(400).json({
+        message: 'Email field is required for verification'
+      });
+    }
+
+    const user = await User.findOne({ userEmail });
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'User not found'
+      });
+    }
+
+    const generatedOTP = () => {
+      return Math.floor(100000 + Math.random() * 900000).toString();
+    };
+    const otp = generatedOTP();
+
+    await transporter.sendMail({
+      from: 'msi.devdixit@gmail.com',
+      to: userEmail,
+      subject: 'OTP for Resetting Password',
+      text: `OTP Verification`,
+      html: `Your verification otp is <b>${otp}</b>`
+    })
+
+    await redis.set(`${user._id}`, JSON.stringify(otp), 'EX', 900);
+
+    const encodeToken = await encodeJwt(user._id);
+
+    res.cookie('token', encodeToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 15 * 60 * 1000
+    });
+
+    return res.status(200).json({
+      message: `We just sent a 6-digit OTP to your email for password reset verification.`
     });
   }
-
-  const user = await User.findOne({ userEmail });
-
-  if (!user) {
-    return res.status(400).json({
-      message: 'User not found'
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
     });
   }
-
-  const generatedOTP = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-  };
-  const otp = generatedOTP();
-
-  await transporter.sendMail({
-    from: 'msi.devdixit@gmail.com',
-    to: userEmail,
-    subject: 'OTP for Resetting Password',
-    text: `OTP Verification`,
-    html: `Your verification otp is <b>${otp}</b>`
-  })
-
-  await redis.set(`${user._id}`, JSON.stringify(otp), 'EX', 900);
-
-  const encodeToken = await encodeJwt(user._id);
-
-  res.cookie('token', encodeToken, {
-    httpOnly: true,
-    secure: true,
-    maxAge: 15 * 60 * 1000
-  });
-
-  return res.status(200).json({
-    message: `We just sent a 6-digit OTP to your email for password reset verification.`
-  });
 } // 8.5/10
 
 export const UserVerifyOtp = async (req, res) => {
-  const { otp } = req.body;
-  const user = req.user;
+  try {
+    const { otp } = req.body;
+    const user = req.user;
 
-  const userDoc = await User.findById(user._id);
-  if (!userDoc) return res.status(404).json({ message: 'User not found' });
-
-
-  const parsedData = await redis.get(`${user._id}`);
-
-  const matchOtp = JSON.parse(parsedData) === otp
-
-  if (!otp) {
-    return res.status(400).json({ message: 'OTP is required' });
-  }
+    const userDoc = await User.findById(user._id);
+    if (!userDoc) return res.status(404).json({ message: 'User not found' });
 
 
-  if (!matchOtp) {
-    return res.status(400).json({
-      message: 'Incorrect OTP'
+    const parsedData = await redis.get(`${user._id}`);
+
+    const matchOtp = JSON.parse(parsedData) === otp
+
+    if (!otp) {
+      return res.status(400).json({ message: 'OTP is required' });
+    }
+
+
+    if (!matchOtp) {
+      return res.status(400).json({
+        message: 'Incorrect OTP'
+      });
+    }
+
+    await redis.del(`${user._id}`);
+
+    return res.status(200).json({
+      message: 'OTP Verified. Now you can change the password',
+      redirect: '/reset-password'
     });
   }
-
-  await redis.del(`${user._id}`);
-
-  return res.status(200).json({
-    message: 'OTP Verified. Now you can change the password',
-    redirect: '/reset-password'
-  });
+  catch (error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
 } // 9/10
 
 export const UserResetPassword = async (req, res) => {
-  const { newPassword, confirmNewPassword } = req.body;
-  const user = req.user;
+  try {
+    const { newPassword, confirmNewPassword } = req.body;
+    const user = req.user;
 
-  if (!newPassword || !confirmNewPassword) {
-    return res.status(400).json({
-      message: 'All fields are required'
+    if (!newPassword || !confirmNewPassword) {
+      return res.status(400).json({
+        message: 'All fields are required'
+      });
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      return res.status(400).json({
+        message: 'Your password must be matched'
+      })
+    }
+
+    if (newPassword.length < 6) {
+      return res.status(400).json({
+        message: 'Password must be at least 6 characters long'
+      });
+    }
+
+
+    const decodeOldPassword = await bcrypt.compare(newPassword, user.password);
+
+    if (decodeOldPassword) {
+      return res.status(400).json({
+        message: 'New password will be different from the old password'
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashPassword;
+    await user.save();
+
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: true
+    });
+
+    return res.status(200).json({
+      message: 'Your password has been reset successfully.',
+      redirectedTo: '/login'
     });
   }
-
-  if (newPassword !== confirmNewPassword) {
-    return res.status(400).json({
-      message: 'Your password must be matched'
-    })
-  }
-
-  if (newPassword.length < 6) {
-    return res.status(400).json({
-      message: 'Password must be at least 6 characters long'
+  catch(error) {
+    console.log(`Server error - ${error}`);
+    return res.status(500).json({
+      message: 'Internal Server Error'
     });
   }
-
-
-  const decodeOldPassword = await bcrypt.compare(newPassword, user.password);
-
-  if (decodeOldPassword) {
-    return res.status(400).json({
-      message: 'New password will be different from the old password'
-    });
-  }
-
-  const hashPassword = await bcrypt.hash(newPassword, 10);
-
-  user.password = hashPassword;
-  await user.save();
-
-  res.clearCookie('token', {
-    httpOnly: true,
-    secure: true
-  });
-
-  return res.status(200).json({
-    message: 'Your password has been reset successfully.',
-    redirectedTo: '/login'
-  });
 } // 9/10
