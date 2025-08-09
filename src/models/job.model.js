98f4d41f0f0613561b6312ff2f179d837cdba0d5
Dev Dixit <devdixitsocial@gmail.com>
2025-08-09 19:52:07 +0530

    modify: user model, add: job model

---
+++ src/models/job.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema({
+  jobTitle: {
+    type: String,
+    required: true
+  },
+  jobPostedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  jobLocation: {
+    type: String,
+    required: true
+  },
+  jobSalaryRange: {
+    type: String
+  },
+  jobSkills: [{
+    type: String,
+    required: true
+  }],
+  jobCategory: {
+    type: String,
+    enum: ['full-time', 'part-time', 'contract']
+  },
+  jobShift: {
+    type: String,
+    enum: ['morning', 'night', 'custom']
+  },
+  jobKeyRes: [{
+    type: String,
+    required: true
+  }],
+  jobReqQual: [{
+    type: String,
+    required: true
+  }],
+  jobFullAddress: {
+    type: String
+  },
+  jobIntType: {
+    type: String,
+    enum: ['in-person', 'online']
+  },
+  jobType: {
+    type: String,
+    enum: ['in-office', 'hybrid', 'remote']
+  }
+});
+
+const Job = mongoose.model('Job', JobSchema);
+
+export default Job;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,20 +2,25 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   fullName: {
-    type: String
+    type: String,
+    required: true
   },
   userEmail: {
-    type: String
+    type: String,
+    required: true
   },
   userContactNumber: {
-    type: Number
+    type: Number,
+    required: true
   },
   userRole: {
     type: String,
+    required: true
   },
   password: {
     type: String,
-    minLength: 6
+    minLength: 6,
+    required: true
   },
   isUserVerified: {
     type: Boolean,
@@ -24,7 +29,13 @@ const UserSchema = new mongoose.Schema({
   isUserLoggedIn: {
     type: Boolean,
     default: false
-  }
+  },
+  appliedJobs: [{
+    type: mongoose.Schema.Types.ObjectId
+  }],
+  postedJobs: [{
+    type: mongoose.Schema.Types.ObjectId
+  }]
 }, {timestamps: true});
 
 const User = mongoose.model('User', UserSchema);
